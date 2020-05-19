6051c70a298a4373927015791dd58fc04b0bdce1
aagabrielas <ana.silvab93@gmail.com>
2020-05-19 19:43:34 -0400

    Swipe para los carruseles en mobile, carrusel no se mueve automaticamente, navbar cambia de color con scroll, navbar colapsa automáticamente en mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,50 @@
-$(function(){
-    $("a").click(function(event){
-      if (this.hash !==""){
-        event.preventDefault();
-  
-        var gato = this.hash;
-  
-        $("html, body").animate({
-          scrollTop:  $(gato).offset().top
-        },800, function(){
+$(function () {
+  $("a").click(function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gato = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(gato).offset().top,
+        },
+        800,
+        function () {
           window.location.hash = gato;
-        });
-      }
-    });
+        }
+      );
+    }
   });
+});
+
+$(".carousel").on("touchstart", function (event) {
+  var xClick = event.originalEvent.touches[0].pageX;
+  $(this).one("touchmove", function (event) {
+    var xMove = event.originalEvent.touches[0].pageX;
+    if (Math.floor(xClick - xMove) > 5) {
+      $(this).carousel("next");
+    } else if (Math.floor(xClick - xMove) < -5) {
+      $(this).carousel("prev");
+    }
+  });
+  $(".carousel").on("touchend", function () {
+    $(this).off("touchmove");
+  });
+});
+
+$(".carousel").carousel({
+  interval: false,
+});
+
+$(window).scroll(function () {
+  if ($("#menu").offset().top > 550) {
+    $("#menu").addClass("bg-rosa");
+  } else {
+    $("#menu").removeClass("bg-none");
+  }
+});
+
+$('.navbar-nav>li>a').on('click', function(){
+  $('.navbar-collapse').collapse('hide');
+});
